4691764fe5f1b3c4ed7ce538757938122c927fdc
Daniel Kalbasi <danial.kalbassI@gmail.com>
2015-03-08 00:57:11 +0800

    raw php

---
+++ skin-natural-skin-need-hydrating.php
@@ -0,0 +1,112 @@
+<!-- Header -->
+<?php
+include('_includes/header.php');
+?>
+
+
+
+<!-- Begin Content -->
+	<div id="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="breadcrumb"> <a href="index.php">Home</a> > <a href="#">Garnier Skin Natural</a> > Sakura White
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="tajuk-besar">
+						<h1>By Skin Need - Hydrating</h1>
+					</div>
+				</div>
+			</div>
+
+<!-- Product Filter-->
+							<?php
+include('_includes/productfilter.php');
+?>
+
+
+<!-- Products Listed -->
+
+			<div class="row">
+
+			
+
+
+					
+
+					
+
+<div class="col-md-4 col-sm-6 tolak-product  1">
+						<div class=" a">
+            				<img src="images/skin-natural/aqua-defense/aqua-defense-moisturizing-essence.png" class='hold' />
+        				</div>
+				        <div class="col-xs-6 b">
+				           <b>Aqua Defense Moisturizing Cleansing Foam</b><p>Hydrates for the whole day and protect our skin from environment aggressors.
+							</p><a class="bar-findout" href="AD-AquaDefenseMoisturizingCleansingFoam.php"> &#9656; Find Out </a>
+       					</div>
+					</div>
+
+
+					
+				
+
+				
+
+					<div class="col-md-4 col-sm-6 tolak-product  1">
+						<div class=" a">
+            				<img src="images/skin-natural/aqua-defense/AquaDefenseNonStopIntensiveHydratingEssence.png" class='hold' />
+        				</div>
+				        <div class="col-xs-6 b">
+				           <b>Aqua Defense Non-Stop Intensive Hydrating Essence</b><p>The moisturizing essence instantly hydrate your skin and you'll feel refreshed.
+
+
+							</p><a class="bar-findout" href="AD-Non-StopIntensiveHydratingEssence.php"> &#9656; Find Out </a>
+       					</div>
+					</div>
+
+					<div class="col-md-4 col-sm-6 tolak-product  1">
+						<div class=" a"><br>
+            				<img src="images/skin-natural/aqua-defense/ADSkinMask.png" class='hold' />
+        				</div>
+				        <div class="col-xs-6 b">
+				           <b>Aqua Defense Hydrating Glowing Essence 2nd Skin Mask</b><p>Intensive tissue mask specifically formulated for dehyrated skin for plumpy radiant skin
+
+
+							</p><a class="bar-findout" href="AD-HydratingGlowingEssence2ndSkinMask.php"> &#9656; Find Out </a>
+       					</div>
+					</div>
+
+				
+
+				
+
+
+
+
+
+				
+
+				
+
+			</div>
+
+			</div>
+		</div>
+
+<!-- End OF Begin Content -->
+
+
+
+
+
+
+
+<?php
+include('_includes/footer.php');
+?>
+
+
+
