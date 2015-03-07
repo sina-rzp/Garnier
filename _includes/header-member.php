a4a0a95561e8ce6724b8b040e96ecd3d795ac8b0
Daniel Kalbasi <danial.kalbassI@gmail.com>
2015-03-07 23:47:58 +0800

    mobile header spec

--- _includes/header-member.php
+++ _includes/header-member.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['email'])){
 	<head>
 		<meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="viewport" content="width=device-width; initial-scale=1.0; minimum-scale=1; maximum-scale=1.0; user-scalable=0;"/>
 	    <meta name="description" content="">
 	    <meta name="author" content="">
 	    <link rel="icon" href="images/favicon.ico">
