78e47a757539609f57ce54e7dfc8bfe9c49c52cb
sina-rzp <sina.rzp@gmail.com>
2015-03-11 17:02:34 +0800

    GSN edited

--- _includes/gsn.php
+++ _includes/gsn.php
@@ -5,20 +5,21 @@
                 <div class="carousel-inner">
                     <div class="item active">
                         <div class="row">
-          
-                            <div class="col-sm-2"><a href="LC-MultiActionWhiteningCreamExtraUVProtectionSP20.php"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedTMMulti-ActionWhiteningSerumCreamSPF20.png" alt="Image" class="img-responsive"></a>
-                            </div>
+
+						<div class="col-sm-2"><a href="LC-MultiActionWhiteningCreamExtraUVProtectionSP20.php"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedTMMulti-ActionWhiteningSerumCreamSPF20.png" alt="Image" class="img-responsive"></a>
+                            </div><!-- yes -->
                             <div class="col-sm-2"><a href="LC-MilkyLighteningDewToner.php"><img src="images/skin-natural/light-complete/LightCompleteMilkyLighteningDewToner.png" alt="Image" class="img-responsive"></a>
-                            </div>
+                            </div><!-- yes -->
                              <div class="col-sm-2"><a href="LC-LighteningPeelOffMask.php"><img src="images/skin-natural/light-complete/LightCompleteMult-ActionWhiteningPeel-offMask.png" alt="Image" class="img-responsive"></a>
-                            </div>
+                            </div><!-- yes -->
                              <div class="col-sm-2"><a href="LC-MultiActionBrighteningScrub.php"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedMulti- ActionBrighteningScrub.png" alt="Image" class="img-responsive"></a>
-                            </div>
+                            </div><!-- yes -->
                             <div class="col-sm-2"><a href="LC-MultiActionBrighteningFoam.php"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedMulti-ActionBrighteningFoam.png" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-2"><a href="#x"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedTMMulti-ActionWhiteningCream8HoursShine.png" alt="Image" class="img-responsive"></a>
-                            </div>
-                          
+                            </div><!-- yes --> 
+                            <div class="col-sm-2"><a href="LC-LightCompleteWhiteSpeedMultiActionWhiteningCream8HoursShine-Free.php"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedTMMulti-ActionWhiteningCream8HoursShine.png" alt="Image" class="img-responsive"></a>
+                            </div><!-- yes -->
+
+						  
                         </div>
                         
                     </div>
@@ -26,9 +27,11 @@
                     <div class="item">
                           <div class="row">
                             
-                            <div class="col-sm-2"><a href="#x"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedTMMulti-ActionWhiteningSerumCreamNight.png" alt="Image" class="img-responsive"></a>
-                            </div>
-
+                            <div class="col-sm-2"><a href="LC-MultiActionWhiteningCreamNightRestore.php"><img src="images/skin-natural/light-complete/LightCompleteWhiteSpeedTMMulti-ActionWhiteningSerumCreamNight.png" alt="Image" class="img-responsive"></a>
+                            </div><!-- yes -->
+                            
+                            <div class="col-sm-2"><a href="LC-Multi-ActionWhiteningFacePowder.php"><img src="images/skin-natural/light-complete/LightCompleteVisibleWhiteningFace PowderSPF18.png" alt="Image" class="img-responsive"></a>
+                            </div><!-- yes --> 
                           
                         </div>
                        
